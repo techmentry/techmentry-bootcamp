04eac9f99c9703327e84ea22ffd0c5d77307a4c7
Sarthak Tickoo <sarthakt@outlook.com>
2021-06-27 22:45:35 -0400

    Day 3 - final

--- src/data-analysis/data_generator.py
+++ src/data-analysis/data_generator.py
@@ -6,6 +6,8 @@ import datetime
 
 # https://www.covid19api.dev/#intro
 
+# creating a static dictionary for all the month in the 3 letter format. as this is the only
+# sure way of getting it correct without having to do a lot of date parsing.
 months = {
     1: "jan",
     2: "feb",
@@ -21,12 +23,17 @@ months = {
     12: "dec"
 }
 
+# create a global variable for the bearer token.
+# what is a bearer token? in simple words, it is a token we get when we authenticate with the server. when we send it to the server with the
 bearer_token = ""
 api_def = None
 
 
 def read_api_def():
+    # we store this API definition in a file just so that whenever there is a change to the API we don't have to touch the code. We can just
+    # change the API endpoint, and the code should work the same way.
     global api_def
+    # read this API definition from the file in the config folder, and then store it for later use.
     api_def_file_path = os.path.join(
         os.path.dirname(__file__), "config", "api-def.json")
     with open(api_def_file_path, "r") as f:
@@ -34,14 +41,29 @@ def read_api_def():
 
 
 def generate_token(force=False):
+    # The covid 19 tracking API we want to use requires us to authenticate with some form of username and password. To this request,
+    # the API returns a bearer token, which in simpler terms is a way for it to know who is making a request and if that person can
+    # use that endpoint. It also helps keep a track of the number of requests a user has made and also manage telemetry.
     global bearer_token
+
+    # the token that the server sends us has a lifetime of ~55 hours. Hence, we don't need to regenerate it. We can just store the token
+    # and load it the next time we bring up our script. However, it is to be noted that once 55 hours are up, we need to regenerate the token.
+    # you can write some code trivially by storing the date and time the token was generated on in the json file itself, and then using it with the code below
+    # to check if the token present is valid or not. If it is not, then you can refresh it. See a simple example below
     token_file_path = os.path.join(
         os.path.dirname(__file__), "config", "token.json")
+    # check if we have a valid token already in the file
     if force == False and os.path.exists(token_file_path):
         with open(token_file_path) as token_file:
-            bearer_token = json.load(token_file)["token"]
-            return
+            # is the time difference between now and the date time the token was fetched > 50 hours? if no, then continue using this token
+            token_details = json.load(token_file)
+            token_load_dt_tm = datetime.datetime.strptime(
+                token_details["timestamp"], "%m/%d/%Y, %H:%M:%S")
 
+            if (datetime.datetime.now() - token_load_dt_tm).seconds < (50 * 3600):
+                return
+
+    # okay we either need to fetch a token from scratch or need a new one since the old one expired
     auth_params = {
         "username": api_def["username"],
         "password": api_def["password"]
@@ -49,12 +71,13 @@ def generate_token(force=False):
 
     auth_response = requests.post(
         url=urljoin(api_def["root_url"], api_def["api_defs"]["gen_token"]), data=auth_params)
-
+    # please take a look at REST response codes - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
     if auth_response.status_code == 200:
         bearer_token = json.loads(
             auth_response.content.decode("utf-8"))["Document"]
         auth_token = {
-            "token": bearer_token
+            "token": bearer_token,
+            "timestamp": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
         }
         with open(token_file_path, "w") as f:
             json.dump(auth_token, f, indent=4)
@@ -65,13 +88,20 @@ def generate_token(force=False):
 
 
 def get_global_daily_report(month: int, year: int):
+    # get today's date information
     today = datetime.datetime.today()
-    if today.year != 2020:
-        if today.year == 2021:
-            if today.month > 6:
-                raise Exception(
-                    "Invalid date range! please enter the year as either 2020 (month range 1-12) or 2021 (month range 1-current month)")
 
+    # check if the month is valid
+    if month not in months.keys():
+        raise Exception(
+            "Invalid month range! please choose a month range between 1-12")
+
+    # check if the date range supplied actually makes sense. Covid data is tabulated from Jan 2020 till today.
+    if year < 2020 or year > today.year or (year == today.year and month > today.month):
+        raise Exception(
+            "Invalid date range! No valid data prior to Jan 2020 or in the future. Please choose a month and year between and including, Jan 2020 and current month and year")
+    
+    # connect to the server to get the data. we also need to 
     api_req_param = api_def["api_defs"]["global_daily_reports"].format(
         mon=str(months[month]), yyyy=year)
     auth_token = {
--- src/data-analysis/main.py
+++ src/data-analysis/main.py
@@ -4,63 +4,113 @@ import data_generator
 import json
 import os
 
+# this file uses the data generator module that we wrote
+# print some information
 print("Starting..")
 
-month = 4
+# some month and year variable to help us get some data
+month = 1
 year = 2021
 
+# generate some data folder paths
 data_folder = os.path.join(os.path.dirname(
     __file__), "data")
+# setup some data file paths string
 data_file_path = os.path.join(
     data_folder, "data_{}{}.json".format(month, year))
 
+# print this information out.
 print("Data folder: {}".format(data_folder))
 
+# check if we already have data for a given year and month. if it is not, then fetch some data, and write that to a file.
 if os.path.exists(data_file_path) == False:
     print("File for given date {}/{} does not exist. Attempting to fetch data from the server..".format(month, year))
+    # use our data generator module
     data_generator.init()
     data = data_generator.get_global_daily_report(month, year)
+    # if the data is correctly fetched, write it to a file. else raise an error
     if data[0] == 200:
         print("Got data successfully!")
         with open(data_file_path, "w") as f:
             json.dump(json.loads(data[1])["Document"], f, indent=4)
             print("Data written at: {}".format(data_file_path))
+    else:
+        raise RuntimeError(
+            "Some problem with getting data: Code {}, Message: {}".format(data[0], data[1]))
 else:
     print("Found a data file for the given date!")
 
+
+def example_1_write_to_excel(data_df):
+    # If you wish to write this data to a excel row-col format, uncomment this line.
+    data_df.to_excel(os.path.join(data_folder, "data_{}{}.xlsx".format(
+        month, year)), engine="xlsxwriter")
+
+
+def example_2_filter_by_province(data_df):
+    # Example 2: filter the data set and get information for Delhi and then plot a bar graph for confirmed, active, recovered cases and the death numbers
+    ################################################################
+    cases_delhi = data_df[data_df["province_state"].isin(["Delhi"])]
+    cases_delhi.plot(x="last_update", y=[
+        "confirmed", "active", "recovered", "deaths"], kind="bar")
+    plt.show()
+    ################################################################
+
+
+def example_3_filter_by_active_cases(data_df):
+    # Example 3: filter the data set and get information for provinces where there are more than a million active cases
+    ################################################################
+    cond_1 = (data_df["active"] > 1000000)
+    cond_2 = data_df["province_state"].str.lower().str.strip() != "0"
+    active_cases_more_than_1m = data_df[cond_1 & cond_2].drop_duplicates(
+        ["combined_key", "active"]
+    )
+    active_cases_more_than_1m.plot(x="combined_key", y="active", kind="bar")
+    plt.show()
+    ################################################################
+
+
+def example_4_subplot_4_cities_subplot(data_df):
+    # Example 4: filter the data set and plot 4 graphs in one figure,
+    ################################################################
+    fig, axes = plt.subplots(2, 2)
+    cases_in_mah = data_df[(
+        data_df["province_state"].str.lower().str.strip() == "maharashtra".lower())]
+    cases_in_mah.plot(ax=axes[0, 0], x="last_update",
+                      y="active", kind="scatter")
+
+    cases_in_eng = data_df[(
+        data_df["province_state"].str.lower().str.strip() == "England".lower())]
+    cases_in_eng.plot(ax=axes[1, 0], x="last_update",
+                      y="active", kind="scatter")
+
+    cases_in_delhi = data_df[(
+        data_df["province_state"].str.lower().str.strip() == "delhi".lower())]
+    cases_in_delhi.plot(ax=axes[0, 1], x="last_update",
+                        y="active", kind="scatter")
+
+    cases_in_ny = data_df[(
+        data_df["province_state"].str.lower().str.strip() == "New york".lower())]
+    cases_in_ny.plot(ax=axes[1, 1], x="last_update",
+                     y="active", kind="scatter")
+    plt.show()
+    ################################################################
+
+
+# load the json data using pandas and operate on it as a dataframe.
 print("Reading data file from: {}".format(data_file_path))
 df = pandas.read_json(data_file_path)
-# cases_delhi = df[df["province_state"].isin(["Delhi"])]
-# cases_delhi.plot(x="last_update", y=[
-#     "confirmed", "active", "recovered", "deaths"], kind="bar")
 
-# cond_1 = (df["active"] > 1000000)
-# cond_2 = df["province_state"].str.lower().str.strip() != "0"
-# active_cases_more_than_10k = df[cond_1 & cond_2].drop_duplicates(
-#     ["combined_key", "active"]
-# )
-# active_cases_more_than_10k.plot(x="combined_key", y="active", kind="bar")
+# example 1: write to excel sheet
+# if you want to avoid writing to an excel sheet, please comment the line below
+example_1_write_to_excel(df)
 
-fig, axes = plt.subplots(2, 2)
+# please comment example 3 and 4 below and then run this.
+# example_2_filter_by_province(df)
 
-cases_in_mah = df[(
-    df["province_state"].str.lower().str.strip() == "maharashtra".lower())]
-cases_in_mah.plot(ax=axes[0, 0], x="last_update", y="active", kind="scatter")
+# please comment example 2 and 4, and then run the code below
+# example_3_filter_by_active_cases(df)
 
-cases_in_eng = df[(
-    df["province_state"].str.lower().str.strip() == "England".lower())]
-cases_in_eng.plot(ax=axes[1, 0], x="last_update",
-                     y="active", kind="scatter")
-
-cases_in_delhi = df[(
-    df["province_state"].str.lower().str.strip() == "delhi".lower())]
-cases_in_delhi.plot(ax=axes[0, 1], x="last_update", y="active", kind="scatter")
-
-cases_in_ny = df[(
-    df["province_state"].str.lower().str.strip() == "New york".lower())]
-cases_in_ny.plot(ax=axes[1, 1], x="last_update", y="active", kind="scatter")
-
-plt.show()
-
-# df.to_excel(os.path.join(data_folder, "data_{}{}.xlsx".format(month, year)), engine="xlsxwriter")
+# please comment example 2, 3 and then run code below
+# example_4_subplot_4_cities_subplot(df)
 # print(df.info())
